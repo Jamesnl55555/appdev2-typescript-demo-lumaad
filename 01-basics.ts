36e8435f5efa2035b096ba021044624833857f56
Jamesnl55555 <Jamesnl55555@gmail.com>
2026-03-27 14:36:54 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -3,9 +3,9 @@
 let userName: string = "Elmer";
 let userAge = 31;
 
-// ...
-
-userAge = "31"
+// I reassign the values wiht mine
+userName = "Jimmy"
+userAge = 1
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
@@ -16,5 +16,15 @@ function add(a: number, b = 5) {
 
 add(10)
 // add('10')
+// The commented out line above is the function with a string parameter, but it will return error since what's in the parameter is a string
+
 add(10, 6)
 // add(10, '6')
+// The commented out line above is the function with a string parameter, but it will return error too since what's in the second parameter is a string
+
+console.log(add(10))
+//this has a return value of 15 since we have 2 parameters
+//we only have 1 value in this one but it will add the second parameter in the function to this one
+
+console.log(add(10, 6))
+//this will return 16 since these 2 parameters override the dataset in the function
